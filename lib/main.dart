9c82a9cad012c1e9b01f1ed520c498e7c5653ddd
epashiq <ashiqashi4715@gmail.com>
2025-05-27 13:32:18 +0530

    initial commit and added dependencies

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:music_service_module/features/view/screens/music_screen.dart';
+import 'package:music_service_module/general/di/injection.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await configureDependency();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MusicScreen(),
+    );
+  }
+}
